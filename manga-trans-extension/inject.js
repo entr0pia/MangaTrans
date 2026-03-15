f68f8d0f26a8b6bf151fbb006f6ec189f62e28c3
entr0pia <30486766+entr0pia@users.noreply.github.com>
2026-03-15 16:32:04 +0800

    Feat: finalize MangaTrans v1.5 with advanced typography, terminology consistency, and deep reading mode support

--- manga-trans-extension/inject.js
+++ manga-trans-extension/inject.js
@@ -1,11 +1,22 @@
 (function() {
     const originalAttachShadow = Element.prototype.attachShadow;
     Element.prototype.attachShadow = function(init) {
-        // 强制将所有 Shadow DOM 设为 open 模式，以便内容脚本访问
-        if (init && init.mode === 'closed') {
-            init.mode = 'open';
-        }
+        if (init && init.mode === 'closed') init.mode = 'open';
         return originalAttachShadow.apply(this, arguments);
     };
-    console.log("[MangaTrans] Shadow DOM Proxy Active");
+
+    // 劫持 History API 捕获 SPA 路由变化
+    const wrapHistory = function(type) {
+        const orig = history[type];
+        return function() {
+            const rv = orig.apply(this, arguments);
+            // 发送自定义事件，通知 content.js
+            window.postMessage({ type: 'MANGA_TRANS_URL_CHANGED', path: location.pathname }, '*');
+            return rv;
+        };
+    };
+    history.pushState = wrapHistory('pushState');
+    history.replaceState = wrapHistory('replaceState');
+
+    console.log("[MangaTrans] Shadow DOM & History Proxy Active");
 })();
